8c0aa3297e45c06307ee11d7e924e82b7022865d
Simpleuser <kirillutusikov123@gmail.com>
2024-02-06 16:30:32 +0500

    Add files via upload

--- git.py
+++ git.py
@@ -1,13 +1,15 @@
 import os
 import requests
 import json
+from zipfile import ZipFile
+import tempfile
+import base64
+
 logintoken = None
 def login(token="",method="var"):
     global logintoken
     if method == "var":
-        print(os.environ)
         logintoken = os.environ.get("GIT_TOKEN",None)
-        print(logintoken)
         if not logintoken:
             raise Exception('No variable found "GIT_TOKEN".')
         
@@ -85,4 +87,58 @@ def createrepo(name,desc="",private=False,homepage="",template=False):
         "private": private,
         "homepage": homepage,
         "is_template": template
+    })
+
+def saverepo(repo, info=False):
+    temp = tempfile.NamedTemporaryFile()
+    temp.close()
+    with open(temp.name, "wb") as f:
+        req = requests.get(f"https://github.com/{repo}/archive/master.zip", stream=True)
+        if info:
+            print(f"Downloading {repo}")
+        size = int(req.headers.get("Content-Length", 0))
+        if not size:
+            size = len(req.content)
+        if info:
+            print(f"Repo Size: {size}B")
+        downloaded = 0
+        for chunk in req.iter_content(chunk_size=2048):
+            if chunk:
+                f.write(chunk)
+                downloaded += len(chunk)
+                percent = int((downloaded / size) * 100)
+                if info:
+                    print(f"<{'='*(percent//2)}{' '*(50-(percent//2))}>{percent}%")
+        if info:
+            print("Extracting..")
+    zf = ZipFile(temp.name, 'r')
+    zf.extractall()
+    zf.close()
+
+def listfiles(repo,branch="main"):
+    req = requests.get(f"https://api.github.com/repos/{repo}/git/trees/{branch}?recursive=1")
+
+    return req.json()
+
+def createfile(repo,path,file,commitmsg=""):
+    with open(file,"rb") as f:
+        data = base64.b64encode(f.read()).decode()
+    req = requests.put(f"https://api.github.com/repos/{repo}/contents/{path}",headers={"Authorization":f"Bearer {logintoken}"},json={
+        "message":commitmsg,
+        "content":data
+    })
+    return req.json()
+def updatefile(repo,path,file,sha,commitmsg=""):
+    with open(file,"rb") as f:
+        data = base64.b64encode(f.read()).decode()
+    req = requests.put(f"https://api.github.com/repos/{repo}/contents/{path}",headers={"Authorization":f"Bearer {logintoken}"},json={
+        "message":commitmsg,
+        "content":data,
+        "sha": sha
+    })
+    return req.json()
+def deletefile(repo,path,sha,commitmsg=""):
+    req = requests.delete(f"https://api.github.com/repos/{repo}/contents/{path}",headers={"Authorization":f"token {logintoken}"},json={
+        "message":commitmsg,
+        "sha":sha
     })
